f1bb36fa7121a3b0322fdbcc07298e9cf95bbea3
Jesse Smith <jesseholden713@gmail.com>
2025-12-02 09:45:03 -0800

    added url to admin config

--- config/admin.ts
+++ config/admin.ts
@@ -1,18 +1,25 @@
 export default ({ env }) => ({
+  url: env('STRAPI_ADMIN_URL', null), // IMPORTANT FOR HTTPS ON RENDER
+
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
+
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
+
   transfer: {
     token: {
       salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
+
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env('ENCRYPTION_KEY', 'default-encryption-key'),
+    sessionKey: env('SESSION_SECRET'), // Strapi v5 needs this too
   },
+
   flags: {
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
